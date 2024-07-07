d886aa3c757afa94502e096645b7cde6cb99d972
Taha Shahid <tahashahid203@gmail.com>
2024-07-07 18:00:20 +0400

    users can now reply to a thread. The reply is also a thread. So you can reply to that too. And so on and so forth

--- app/(root)/thread/[id]/page.tsx
+++ app/(root)/thread/[id]/page.tsx
@@ -4,6 +4,7 @@ import { fetchUser } from "../../../../lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../../../../components/cards/ThreadCard";
 import { fetchThreadById } from "../../../../lib/actions/thread.actions";
+import Comment from "../../../../components/forms/Comment";
 
 
 const Page = async({ params }: { params: { id: string } }) => {
@@ -35,6 +36,30 @@ const Page = async({ params }: { params: { id: string } }) => {
            createdAt={thread.createdAt}
            comments={thread.children}
                 />
+        </div>
+
+        <div className="mt-7">
+            <Comment
+                threadId={thread.id}
+                currentUserImg={userInfo.image}
+                currentUserId={JSON.stringify(userInfo._id)}
+            />
+        </div>
+
+        <div className="mt-10">
+            {thread.children.map((comment) => (
+                <ThreadCard
+                    id={comment._id}
+                    currentUserId = {user?.id || ""}
+                    parentId={comment.parentId}
+                    content={comment.text}
+                    author={comment.author}
+                    community={comment.community}
+                    createdAt={comment.createdAt}
+                    comments={comment.children}
+                    isComment={true}
+                     />
+            ))}
         </div>
     </section>
     )
