8f891993b82dbf2f513efb211e217eb52e80dbc3
Taha Shahid <tahashahid203@gmail.com>
2024-07-07 23:50:45 +0400

    Implemented the Activities page. Clicking on a particular activity will take you to the thread where you received a reply. The user who replied to your thread will also be shown in the activity notification

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -144,3 +144,27 @@ export async function fetchUsers ({
         throw new Error(`Error fetching users: ${error}`);
     }
 }
+
+export async function getActivity(userId: string) {
+    try {
+        connectToDB();
+
+        const userThreads = await Thread.find({ author: userId });
+
+        const childThreadIds = userThreads.reduce((acc, thread) => {
+            return acc.concat(thread.children);
+        },[])
+
+        const replies = await Thread.find({ _id: { $in: childThreadIds}, author: { $ne: userId} } )
+        .populate({
+            path: 'author',
+            model: User,
+            select: 'name image _id'
+        });
+
+        return replies;
+    }
+    catch (error) {
+        throw new Error(`Error fetching activity: ${error}`);
+    }
+}
